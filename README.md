ee513afd3f537023ef0b254875f62d5a6d8daf4e
Павел Саенко <pasha03.92@mail.ru>
2020-10-15 20:00:52 +0300

    udp support added

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ It replicates a data from the one incoming connection to an others.
 # Using  
 -i : The port for a source connection  
 -b : The port for clients  
-For example: ```python tcp_broadcast.py -i 200 -b 555```
+-t : The protocol of input connection (There are two options tcp(default) or udp)
+For example: ```python tcp_broadcast.py -i 200 -b 555``` - everything that will be received from tcp connection on 200 port will be broadcasted to all tcp clients, connected to 555 port
+or           ```python tcp_broadcast.py -i 200 -t udp -b 555``` - everything that will be received from udp connection on 200 port will be broadcasted to all tcp clients, connected to 555 port
--- tcp_broadcast.py
+++ tcp_broadcast.py
@@ -16,12 +16,13 @@ def try_close (socket):
         pass
 
 class BroadcastServer ():
-    def __init__ (self, input_port, broadcast_port):
-        # type (int , int) -> None
-        self.input_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__ (self, input_port, input_proto, broadcast_port):
+        # type (int ,int, int) -> None
+        self.input_socket = socket.socket(socket.AF_INET, input_proto)
         self.broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.clients = []
         self.input_port = input_port
+        self.input_proto = input_proto
         self.broadcast_port = broadcast_port
         self.work = True
 
@@ -33,37 +34,50 @@ class BroadcastServer ():
     def stop (self):
         self.work = False
 
+    def _wait_input_connection (self):
+        input_conn, _ = self.input_socket.accept()
+        input_conn.settimeout(30) # If connection with the input socket will be lost, and don`t closed properly, 
+                                    # we will never  know that. So we will be blocked in "recv" method forever. So we have to set 30 sec timeout.
+                                    # When timeout will be reached, an exception will be raised and breaks reading loop. 
+                                    # Sщ we will be able to accept an input connection again
+        logging.debug("Input connection accepted {0}".format(input_conn.getsockname()))
+        return input_conn
+
+
     def _broadcast (self):
         """
             The main thread. When input connection (data provider) will be accepted, "reading-broadcasting" loop will be start.
             But raised exceptions will breaks the loop so we will be able to accept new input connection
         """
-        self.input_socket.bind(('',self.input_port))
-        self.input_socket.listen(0)
+        self.input_socket.bind(('0.0.0.0',self.input_port))
+        if (self.input_proto == socket.SOCK_STREAM):
+            self.input_socket.listen(0)
+       
+        if (self.input_proto == socket.SOCK_STREAM):
+            input_conn = self._wait_input_connection()
+
         while self.work:
-            input_conn, _ = self.input_socket.accept()
-            input_conn.settimeout(30) # If connection with the input socket will be lost, and don`t closed properly, 
-                                # we will never  know that. So we will be blocked in "recv" method forever. So we have to set 30 sec timeout.
-                                # When timeout will be reached, an exception will be raised and breaks reading loop. 
-                                # Sщ we will be able to accept an input connection again
-            logging.debug("Input connection accepted {0}".format(input_conn.getsockname()))
-            while self.work:
-                try:
+            try:
+                if (self.input_proto == socket.SOCK_STREAM):
                     data = input_conn.recv(1024)
-                    if not data:
-                        raise Exception ("Connection is broken")
-                    
-                    for client in self.clients[:]:
-                        try:
-                            client.sendall(data)
-                        except Exception as e:
-                            logging.error("Error occured while sending data to client. Reason: {0}".format(e))
-                            try_close(client)
-                            self.clients.remove(client)
-                except Exception as e:
+                else:
+                    data, _ = self.input_socket.recvfrom(1024)
+                if not data and self.input_proto == socket.SOCK_STREAM:
+                    raise Exception ("Connection is broken")
+                
+                for client in self.clients[:]:
+                    try:
+                        client.sendall(data)
+                    except Exception as e:
+                        logging.error("Error occured while sending data to client. Reason: {0}".format(e))
+                        try_close(client)
+                        self.clients.remove(client)
+            except Exception as e:
+                if (self.input_proto == socket.SOCK_STREAM):
                     try_close(input_conn) 
                     logging.error("Error occured while reading data. Reason: {0}".format(e))
-                    break
+                    input_conn = self._wait_input_connection()
+                
 
     def _listen_for_clients (self):
         """
@@ -82,13 +96,21 @@ class PortAction(argparse.Action):
         if not 0 < values < 2**16:
             raise argparse.ArgumentError(self, "port numbers must be between 0 and 65535")
         setattr(namespace, self.dest, values)
+
+class ProtoAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values != "tcp" and values != "udp":
+            raise argparse.ArgumentError(self, "protocol should be tcp or udp")
+        setattr(namespace, self.dest, values)
   
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", help="The port for input connection", default=200, action=PortAction, type = int)
     parser.add_argument("-b", help="The broadcast port", default=666,action=PortAction, type = int)
+    parser.add_argument("-t", help="The input protocol", default="tcp", action=ProtoAction)
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
-    BroadcastServer (args.i,args.b).start()
+    proto = socket.SOCK_STREAM if args.t == "tcp" else socket.SOCK_DGRAM
+    BroadcastServer (args.i,proto,args.b).start()
             
